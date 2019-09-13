b512fbc05151aaaad3163120f84e8fbece3c8fcf
Mitchell O'Hair <mitchellohair@gmail.com>
2019-09-13 16:29:26 -0700

    Added basic Link integration and basic token exchange

--- app/index.js
+++ app/index.js
@@ -15,8 +15,38 @@ const PLAID_ENV = 'sandbox';
 const PLAID_PRODUCTS = 'auth,transactions,balance,identity';
 const PLAID_COUNTRY_CODES = 'US';
 
+var PUBLIC_TOKEN = '';
+var ACCESS_TOKEN = '';
+
 // Initialize client
 const plaidClient = new plaid.Client(PLAID_CLIENT_ID,
                                      PLAID_SECRET,
                                      PLAID_PUBLIC_KEY,
                                      plaid.environments[PLAID_ENV]);
+
+const app = express();
+app.use(express.static(__dirname + '/../public'));
+app.listen(APP_PORT);
+// app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+// Accept the public_token sent from Link
+app.post('/get_access_token', function (request, response) {
+  PUBLIC_TOKEN = request.body.public_token;
+  plaidClient.exchangePublicToken(PUBLIC_TOKEN, function (error,
+    tokenResponse) {
+    if (error != null) {
+      console.log('Could not exchange public_token!' + '\n' +
+        error);
+      return response.json({ error: error });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    var ITEM_ID = tokenResponse.item_id;
+    console.log('Access Token: ' + ACCESS_TOKEN);
+    console.log('Item ID: ' + ITEM_ID);
+    response.json({ 'error': null });
+  });
+});
