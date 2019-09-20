b86e420553b1d6a3287ffe192fb74be205ffc9ec
Mitchell O'Hair <mitchellohair@gmail.com>
2019-09-20 09:49:00 -0700

    Upon loading, server sends public key to initial component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Link from './Link';
 import Dash from './Dash';
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App public_key='7bf1d2c9b6d6fd3df039a11ef9a29a' />, document.getElementById('root'));
+// Get public key & render App component
+(function () {
+  var request = new XMLHttpRequest();
+  request.open('POST', '/');
+  request.responseType = 'json';
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      ReactDOM.render(<App public_key={request.response.public_key} />, document.getElementById('root'));
+    }
+  };
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+})();
