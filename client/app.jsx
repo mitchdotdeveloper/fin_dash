2fa3f4be2dcd7f1dd49493f3073fb0be8599fb30
Mitchell O'Hair <mitchell.ohair@gmail.com>
2020-03-22 11:42:49 -0700

    App component subscribes to authReducer

--- client/app.jsx
+++ client/app.jsx
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from './redux/actions';
+import { useSelector } from 'react-redux';
 
 import Auth from './components/auth';
 import Dash from './components/dashboard/dash';
 import './styles/app.css';
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [auth, setAuthentication] = useState(false);
-
-  const authenticate = (user=null) => {
-    setUser(user);
-    setAuthentication(!!user);
-  }
+  const auth = useSelector(state => state.authReducer);
 
   return (
     <>
-      {auth
-        ? <Dash user={user} logout={authenticate} />
-        : <Auth login={authenticate} />}
+      {auth.authenticated ? <Dash /> : <Auth />}
     </>
   );
 }
+
+// const App = () => {
+//   const [user, setUser] = useState(null);
+//   const [auth, setAuthentication] = useState(false);
+
+//   const authenticate = (user=null) => {
+//     setUser(user);
+//     setAuthentication(!!user);
+//   }
+
+//   return (
+//     <>
+//       {auth
+//         ? <Dash user={user} logout={authenticate} />
+//         : <Auth login={authenticate} />}
+//     </>
+//   );
+// }
 
 export default App;
